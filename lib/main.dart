541916022846bbdf967a6e4518a7141a133dcccd
dayeon <dayeonko@outlook.com>
2024-07-21 13:55:39 +0900

    "Chapter Widgets"

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  void userTapped(){
+    print("User tapped!");
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.blue,
-        appBar: AppBar(
-          title: Text("My App Bar"),
-          backgroundColor: Colors.deepPurple,
-          elevation: 0,
-          leading: Icon(Icons.menu),
-          actions:[
-            IconButton(onPressed: (){}, icon: Icon(Icons.logout)),
-          ],
-        ),
         body: Center(
-          child: Container(
-            height: 300,
-            width: 300,
-            color: Colors.deepPurple,
-            decoration:BoxDecoration(
-              color: Colors.deepPurple,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            padding:EdgeInsets.all(25),
-            child: Icon(
-              Icons.favorite,
-              color: Colors.white,
-              size: 64,
+          child: GestureDetector(
+            onTap: userTapped,
+            child: Container(
+              height: 200,
+              width: 200,
+              color: Colors.deepPurple[300],
+              child: Center(child: Text("Tap me!")),
             ),
           ),
-        ),
+        )
       ),
     );
   }
