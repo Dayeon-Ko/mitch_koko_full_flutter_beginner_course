adffd18596a78a3ff3ec57cf545faf8ec656f2f4
dayeon <dayeonko@outlook.com>
2024-07-22 14:04:47 +0900

    "Navigation Widgets Part 2 (Bottom Navigation Bar) edit"

---
+++ android/app/src/main/kotlin/com/example/mitch_koko_full_flutter_beginner_course/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mitch_koko_full_flutter_beginner_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:mitch_koko_full_flutter_beginner_course/pages/first_page.dart';
-import 'package:mitch_koko_full_flutter_beginner_course/pages/home_page.dart';
-import 'package:mitch_koko_full_flutter_beginner_course/pages/settings_page.dart';
 
 void main() {
   runApp(MyApp());
